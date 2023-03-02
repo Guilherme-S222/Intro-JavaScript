75c24a1ff3bc7ef4c13a7396527e112c41404f1c
Guilherme-S222 <124743404+Guilherme-S222@users.noreply.github.com>
2023-03-02 20:32:23 -0300

    aula-estrutura-condicional

---
+++ exemplos/003-estrutura-condicional.js
@@ -0,0 +1,38 @@
+let entrada = require('prompt-sync')();
+
+let idade = entrada('Digite sua idade ');
+
+if (idade == 18){
+    console.log('Você pode se matricular na auto-escola');
+
+}
+
+if (idade > 18){
+    console.log('Você tem idade para dirigir caso tenha a habilitação');
+
+}
+if (idade < 18){
+    console.log('Você não tem idade para dirigir');
+
+}
+console.log('A idade digitada é: ' + idade);
+
+// console.log ('A idade digitada é: ' + idade);
+// nesse código a seguir você deve pensar na probabilidade de pessoas maiores de 18 e menores de 18, qual é maior? Então sabendo disso primeiro pergunte a maior probabilidade para deixar o código mais eficiênte;
+
+if (idade > 18){
+    console.log('Você tem idade para dirigir caso tenha a habilitação');
+}
+else if (idade < 18){
+    console.log('Você não tem idade para dirigir caso tenha a habilitação');
+}
+else {
+    console.log('Você pode se matricular na auto-escola');
+}
+
+if (idade >= 18) {
+    console.log('Maior');
+} 
+else{
+    console.log('Menor');
+}
