4d245252d0daddc34d74dfcef7c22c9689ccecc2
Guilherme-S222 <124743404+Guilherme-S222@users.noreply.github.com>
2023-03-02 20:34:40 -0300

    Update 003-estrutura-condicional.js

--- exemplos/003-estrutura-condicional.js
+++ exemplos/003-estrutura-condicional.js
@@ -4,16 +4,12 @@ let idade = entrada('Digite sua idade ');
 
 if (idade == 18){
     console.log('Você pode se matricular na auto-escola');
-
 }
-
 if (idade > 18){
     console.log('Você tem idade para dirigir caso tenha a habilitação');
-
 }
 if (idade < 18){
     console.log('Você não tem idade para dirigir');
-
 }
 console.log('A idade digitada é: ' + idade);
 
@@ -29,7 +25,6 @@ else if (idade < 18){
 else {
     console.log('Você pode se matricular na auto-escola');
 }
-
 if (idade >= 18) {
     console.log('Maior');
 } 
