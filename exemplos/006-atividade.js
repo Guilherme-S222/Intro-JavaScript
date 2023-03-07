d0d1a409b40251b5082850e00b139730e8096a70
Guilherme-S222 <124743404+Guilherme-S222@users.noreply.github.com>
2023-03-07 20:49:26 -0300

    Update 006-atividade.js

--- exemplos/006-atividade.js
+++ exemplos/006-atividade.js
@@ -3,3 +3,31 @@
     SE O VALOR INICIAL FOR MENOR QUE O FINAL EXIBA UMA CONTAGEM CRESCENTE 
     SE O VALOR INICIAL FOR MAIOR QUE O FINAL EXIBA A CONTAGEM DECRESCENTE 
 */
+let entrada = require('prompt-sync')();
+
+console.log('Atividade estrutura de repetição');
+let valor = '';
+valor1 = entrada('Digite o valor inicial ');
+let n1 = parseInt(valor);
+valor2 = entrada('Digite o valor final ');
+let n2 = parseInt(valor);
+
+let contador = n1;
+
+console.log('for');
+
+if (valor1 < valor2){
+
+
+
+for (contador = n1; contador <= n2; contador++) {
+    console.log(contador);
+}
+}
+
+console.log('Os valor de entrada foram: ');
+console.log('Valor inicial: ' + contador + '\n' + 'Valor final: ' + n2);
+
+entrada();
+
+
